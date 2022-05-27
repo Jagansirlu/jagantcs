5b6e2ba6d531c55e0c9f8072113a7645fc112540
jagan sirlu <jagansirlu021@gmail.com>
2022-05-27 13:51:45 +0000

    project implementation

---
+++ project implementation/pensionmangementsystem/src/main/java/com/demo/service/PensionerService.java
@@ -0,0 +1,7 @@
+package com.demo.service;
+
+public interface PensionerService {
+
+	//GET
+	public <Pensioner> Pensioner getPensionerDetailByAadhaar(Pensioner pensioner);
+}
---
+++ project implementation/pensionmangementsystem/src/main/java/com/demo/serviceimpl/pensionSericeImpl.java
@@ -0,0 +1,13 @@
+package com.demo.serviceimpl;
+
+import com.demo.service.PensionerService;
+
+public class pensionSericeImpl implements PensionerService {
+
+	@Override
+	public <Pensioner> Pensioner getPensionerDetailByAadhaar(Pensioner pensioner) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
