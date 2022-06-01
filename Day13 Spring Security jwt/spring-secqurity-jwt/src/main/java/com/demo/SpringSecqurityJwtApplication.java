cb51be611b20db8a5ad709a00c440e40a2d9b469
jagan sirlu <jagansirlu021@gmail.com>
2022-06-01 04:45:06 +0000

    spring security jwt

---
+++ Day13 Spring Security jwt/spring-secqurity-jwt/src/main/java/com/demo/SpringSecqurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecqurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecqurityJwtApplication.class, args);
+	}
+
+}
